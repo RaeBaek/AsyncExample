fa15e7ad170136539322d5618c327d0c8e8d9370
RaeHoon Baek <bmp117@naver.com>
2023-02-16 12:42:10 +0900

    네이버 boostcource <iOS 앱 프로그래밍> / 4. 내 사진 관리 애플리케이션 / PhotoExample / 2. 2) OperationQueue ~ 3) OperationQueue 활용

--- AsyncExample/ViewController.swift
+++ AsyncExample/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func touchUpDownloadButton(_ sender: UIButton) {
+        // 이미지 다운로드 -> 이미지 뷰에 셋팅
+        //https://img.redbull.com/images/w_3000/q_auto,f_auto/redbullcom/2016/08/11/1331811639859_2/gallery-tahiti-adriano-de-souza-standing-tall-in-the-tube
+        
+        guard let imageURL: URL = URL(string: "https://img.redbull.com/images/w_3000/q_auto,f_auto/redbullcom/2016/08/11/1331811639859_2/gallery-tahiti-adriano-de-souza-standing-tall-in-the-tube") else {
+            return
+        }
+        
+        OperationQueue().addOperation {
+            do {
+                let imageDate: Data = try Data.init(contentsOf: imageURL)
+                guard let image: UIImage = UIImage(data: imageDate) else {
+                    return
+                }
+                OperationQueue.main.addOperation {
+                    self.imageView.image = image
+                }
+            } catch {
+                return
+            }
+        }
+    }
 }
-
